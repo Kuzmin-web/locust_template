48bc54c59eb0926beb608414afce359048f3a455
Илья Кузьмин <kuzmin.ia19@gmail.com>
2026-01-18 19:27:58 +1000

    Modifed base scenario

--- user_classes/wt_base_scenario.py
+++ user_classes/wt_base_scenario.py
@@ -1,26 +1,61 @@
 from locust import task, SequentialTaskSet, HttpUser, constant_pacing, events, FastHttpUser
-from config.config import cfg, logger
 import sys
+from config.config import cfg, logger
+
 
 
 class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами (содержит основной сценарий)
     @task()
     def uc_00_getHomePage(self):
-        r00_01_responce = self.client.get(
+        r00_01_response = self.client.get(
             '/WebTours/',
-            name="r00_01_responce",
+            name="r00_01_response",
             allow_redirects=False,
             headers={
                 'sec-ch-ua': '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
                 'sec-ch-ua-mobile': '?0'
             },
-            debug_stream=sys.stdout
+            debug_stream=sys.stderr
         )
-        logger.info(f"Статус ответа: {r00_01_responce.status_code}, Тело ответа {r00_01_responce.text}")
-        # print(f"Статус ответа: {r00_01_responce.status_code}, Тело ответа {r00_01_responce.text}")
+
+
+        r00_02_header_html = self.client.get(
+            '/WebTours/header.html',
+            name="r00_02_header_html",
+            allow_redirects=False,
+            debug_stream=sys.stderr
+        )
         
 
-class WebToursBaseUserClass(HttpUser): # юзер-класс, принимающий в себя основные параметры теста
+        r00_03_welcome_pl = self.client.get(
+            '/cgi-bin/welcome.pl?signOff=true',
+            name="r00_03_welcome_pl_html",
+            allow_redirects=False,
+            debug_stream=sys.stderr
+        )
+
+
+        r00_04_navi_pl = self.client.get(
+            '/cgi-bin/nav.pl?in=home',
+            name="r00_04_navi_pl",
+            allow_redirects=False,
+            debug_stream=sys.stderr
+        )
+
+
+        r00_05_home_html = self.client.get(
+            '/cgi-bin/nav.pl?in=home',
+            name="r00_05_home_html",
+            allow_redirects=False,
+            debug_stream=sys.stderr
+        )       
+
+
+        # logger.info(f"Статус ответа: {r00_01_response.status_code}, Тело ответа {r00_01_response.text}")
+        # print(f"Статус ответа: {r00_01_response.status_code}, Тело ответа {r00_01_response.text}")
+        
+
+class WebToursBaseUserClass(FastHttpUser): # юзер-класс, принимающий в себя основные параметры теста
     wait_time = constant_pacing(cfg.pacing)
     host = cfg.url
 
