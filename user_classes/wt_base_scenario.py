d211b8215314d16fa67d3250003a70c669040e1d
Илья Кузьмин <kuzmin.ia19@gmail.com>
2026-01-18 18:01:29 +1000

    modifet wt_base

--- user_classes/wt_base_scenario.py
+++ user_classes/wt_base_scenario.py
@@ -1,5 +1,6 @@
-from locust import task, SequentialTaskSet, HttpUser, constant_pacing, events
+from locust import task, SequentialTaskSet, HttpUser, constant_pacing, events, FastHttpUser
 from config.config import cfg, logger
+import sys
 
 
 class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами (содержит основной сценарий)
@@ -12,10 +13,11 @@ class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами 
             headers={
                 'sec-ch-ua': '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
                 'sec-ch-ua-mobile': '?0'
-            }
+            },
+            debug_stream=sys.stdout
         )
         logger.info(f"Статус ответа: {r00_01_responce.status_code}, Тело ответа {r00_01_responce.text}")
-        print(f"Статус ответа: {r00_01_responce.status_code}, Тело ответа {r00_01_responce.text}")
+        # print(f"Статус ответа: {r00_01_responce.status_code}, Тело ответа {r00_01_responce.text}")
         
 
 class WebToursBaseUserClass(HttpUser): # юзер-класс, принимающий в себя основные параметры теста
