89d8167ec3e6ea27ff32077586da63f6b2dd2f29
Илья Кузьмин <kuzmin.ia19@gmail.com>
2026-01-24 08:13:02 +1000

    BugFix_PaymentDetails

--- user_classes/wt_base_scenario.py
+++ user_classes/wt_base_scenario.py
@@ -3,6 +3,7 @@ import sys, re, random
 from config.config import cfg, logger
 from utils.assertion import check_http_response
 from utils.non_test_methods import open_csv_file, open_random_csv_file, generateFlightDate
+from urllib.parse import quote_plus
 
 class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами (содержит основной сценарий)
 
@@ -164,19 +165,76 @@ class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами 
         
         self.departDate, self.returnDate = generateFlightDate()
         
-        logger.info(f"______________DEPART DATE: {self.departDate}, RETURN DATE: {self.returnDate}")
+        # logger.info(f"______________DEPART DATE: {self.departDate}, RETURN DATE: {self.returnDate}")
 
         self.body_r03_01_reservations_pl = f'advanceDiscount=0&depart={self.depart}&departDate={self.departDate}&arrive={self.arrive}&returnDate={self.returnDate}&numPassengers=1&seatPref={self.seatPref}&seatType={self.seatType}&findFlights.x=49&findFlights.y=9&.cgifields=roundtrip&.cgifields=seatType&.cgifields=seatPref'
-        logger.info(f"______________DATA_R03_01: {self.body_r03_01_reservations_pl}")  
+        # logger.info(f"______________DATA_R03_01: {self.body_r03_01_reservations_pl}")  
   
         with self.client.post(
             '/cgi-bin/reservations.pl',
             name="r03_01_reservations_pl",
+            data=self.body_r03_01_reservations_pl,
+            headers=self.headers,
             allow_redirects=False,
             # debug_stream=sys.stderr,
             catch_response=True
         ) as r03_01_reservations_pl:
-            check_http_response(r03_01_reservations_pl, "Find Flight")
+            check_http_response(r03_01_reservations_pl, "Flight Selections")
+            
+        self.dict_outboundFlight = re.findall(r'name="outboundFlight"\s+value="([^"]*)"', r03_01_reservations_pl.text)
+        
+        # logger.info(f"______________SELECTED FLIGHT: {self.dict_outboundFlight}")
+    
+    
+    @task()
+    def uc_04_ChiooseFlightsOptions(self):
+        
+        self.seatType = self.random_flights_row["seatType"]
+        self.seatPref = self.random_flights_row["seatPref"]
+        
+        self.outboundFlight = random.choice(self.dict_outboundFlight)
+
+        self.body_uc_04_01_ChiooseFlightsOptions = f'outboundFlight={self.outboundFlight}&numPassengers=1&advanceDiscount=0&seatType={self.seatType}&seatPref={self.seatPref}&reserveFlights.x=57&reserveFlights.y=10'
+
+        # logger.info(f"______________DATA_uc_04_01_ChiooseFlightsOptions: {self.body_uc_04_01_ChiooseFlightsOptions}")
+         
+        with self.client.post(
+            '/cgi-bin/reservations.pl',
+            name="uc_04_01_ChiooseFlightsOptions",
+            data=self.body_uc_04_01_ChiooseFlightsOptions,
+            headers=self.headers,
+            allow_redirects=False,
+            # debug_stream=sys.stderr,
+            catch_response=True
+        ) as uc_04_01_ChiooseFlightsOptions:
+            check_http_response(uc_04_01_ChiooseFlightsOptions, "Flight Reservation")        
+
+            
+    @task()
+    def uc_05_PaymentDetails(self):
+        
+        self.creditCard = self.random_flights_row["creditCard"]
+        self.expDate = self.random_flights_row["expDate"]
+        self.adress1 = self.random_user_row["address1"]
+        self.adress2 = self.random_user_row["address2"]
+        self.pass1 = self.random_user_row["pass1"]
+
+        self.body_uc_05_01_PaymentDetails = f'firstName={self.login}&lastName={self.password}&address1={self.adress1}&address2={self.adress2}&pass1=jojo+bean&creditCard={self.creditCard}&expDate={self.expDate}&oldCCOption=&numPassengers=1&seatType={self.seatType}&seatPref={self.seatPref}&outboundFlight={self.outboundFlight}&advanceDiscount=0&returnFlight=&JSFormSubmit=off&buyFlights.x=76&buyFlights.y=11&.cgifields=saveCC'
+
+        # logger.info(f"______________DATA_uc_05_01_PaymentDetails: {self.body_uc_05_01_PaymentDetails}")
+         
+        with self.client.post(
+            '/cgi-bin/reservations.pl',
+            name="uc_05_01_PaymentDetails",
+            data=self.body_uc_05_01_PaymentDetails,
+            headers=self.headers,
+            allow_redirects=False,
+            # debug_stream=sys.stderr,
+            catch_response=True
+        ) as uc_05_01_PaymentDetails:
+            check_http_response(uc_05_01_PaymentDetails, "Thank you for booking through Web Tours.")                        
+            
+        
 
         # logger.info(f"Статус ответа: {r00_01_response.status_code}, Тело ответа {r00_01_response.text}")
         # print(f"Статус ответа: {r00_01_response.status_code}, Тело ответа {r00_01_response.text}")
